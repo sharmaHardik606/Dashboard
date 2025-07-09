9261d9085a63773018396a7fe72d73580e2992e2
Hardik Sharma <sharmahardik606@gmail.com>
2025-07-09 18:03:14 +0530

    problem causing typo solved

--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-white text-black">
       <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="text-lg mb-6">Sorry, the page you're looking for doesn't exist.</p>
+      <p className="text-lg mb-6">Sorry, the page you are looking for does not exist.</p>
       <Link href="/" className="text-blue-600 underline hover:text-blue-800">
         Go back home
       </Link>
