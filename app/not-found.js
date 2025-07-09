38b94c3cdb28d89f42e937740441e77f76b4d0ad
Hardik Sharma <sharmahardik606@gmail.com>
2025-07-09 18:00:15 +0530

    added custom 404 not found page

---
+++ app/not-found.js
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-white text-black">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-6">Sorry, the page you're looking for doesn't exist.</p>
+      <Link href="/" className="text-blue-600 underline hover:text-blue-800">
+        Go back home
+      </Link>
+    </div>
+  );
+}
